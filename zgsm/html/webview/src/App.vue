7e672fa7977066b6bf8513d96f1dc66f4052c0a4
Liu Wei <monkeydluffy6017@gmail.com>
2025-04-11 14:11:59 +0800

    fix: activitybar (#4)

--- zgsm/html/webview/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import Home from '@/views/home/index.vue'
-</script>
-
-<template>
-  <Home />
-</template>
--- zgsm/html/webview/src/utils/scroll.ts
+++
@@ -1,6 +0,0 @@
-export function scrollToBottom() {
-    const chatList = document.getElementById("chatList");
-    if (chatList) {
-        chatList.scrollTop = chatList.scrollHeight;
-    }
-}
