2b7f17cd09adec0b72725938d7fbbb2aa641b65a
mini2s <xlm154486154@outlook.com>
2025-06-27 15:37:21 +0800

    refactor(ipc/utils): Simplify IPC path generation and add .sock extension

--- src/zgsmAuth/ipc/utils.ts
+++ src/zgsmAuth/ipc/utils.ts
@@ -4,7 +4,7 @@ import * as vscode from "vscode"
 
 export function getIPCPath() {
 	// const sid = `${vscode.env.machineId}-${vscode.env.uriScheme}-shenma-login-sync`
-	const sid = `${vscode.env?.machineId ?? "jFWyVy9wUKKSkX55TDBt2SuQWl7fDM1l"}-${vscode.env?.uriScheme ?? "vscode"}-shenma-login-sync`
+	const sid = `${vscode.env?.uriScheme ?? "vscode"}-shenma-login-sync`
 	// Use a unique socket path for the IPC server
-	return process.platform === "win32" ? `\\\\.\\pipe\\${sid}` : path.join(os.tmpdir(), sid)
+	return process.platform === "win32" ? `\\\\.\\pipe\\${sid}` : path.join(os.tmpdir(), `${sid}.sock`)
 }
