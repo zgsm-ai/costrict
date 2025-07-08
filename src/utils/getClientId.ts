61a899c7c0fa408ea90abbf6d7c7c6ba7001ceaf
mini2s <1942696552@qq.com>
2025-07-09 03:43:22 +0800

    fix(utils): change delimiter from pipe to dot in client ID generation

--- src/utils/getClientId.ts
+++ src/utils/getClientId.ts
@@ -2,5 +2,5 @@ import * as vscode from "vscode"
 import * as os from "os"
 
 export const getClientId = () => {
-	return `${vscode.env.machineId}${vscode.env.remoteName ? `|${os.hostname()}` : ""}`
+	return `${vscode.env.machineId}${vscode.env.remoteName ? `.${os.hostname()}` : ""}`
 }
